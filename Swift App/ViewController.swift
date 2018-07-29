db59c6bafbddf3d0ae94e5e8f8ed690189630dd5
Franscois Bornman <f.bornman@hotmail.co.za>
2018-07-29 17:26:45 +0200

    My first commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Franscois Bornman on 2018/07/29.
+//  Copyright © 2018 Franscois Bornman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var theLabel: UILabel!
+    
+    
+    var tapCount = 0
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        print(tapCount)
+        
+        if tapCount >= 10 {
+         theLabel.text = "You tapped the button 10 times"
+        }
+        
+    }
+    
+    
+    @IBAction func buttonTapped2(_ sender: Any) {
+        
+        theLabel.text = "Buttons are cool!"
+    
+        
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
